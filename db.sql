e5f7db66793abe9648494025f1647345ff9b30ca
HIBA53757 <hibatallahsadiki20@gmail.com>
2026-01-11 20:29:27 +0100

    WAL-3 Création de la base de données et des tables

---
+++ db.sql
@@ -0,0 +1,53 @@
+
+
+CREATE DATABASE IF NOT EXISTS wallet;
+USE wallet;
+
+
+--@block
+
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE wallet (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    month INT NOT NULL,
+    year INT NOT NULL,
+    budget DECIMAL(10,2) NOT NULL DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (user_id, month, year),
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+);
+
+CREATE TABLE category (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+INSERT INTO category (name) VALUES
+('Nourriture'),
+('Transport'),
+('Loyer'),
+('Loisirs'),
+('Autre');
+
+CREATE TABLE expense (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    wallet_id INT NOT NULL,
+    category_id INT NOT NULL,
+    title VARCHAR(150) NOT NULL,
+    amount DECIMAL(10,2) NOT NULL CHECK (amount >= 0),
+    expense_date DATE NOT NULL,
+    is_automatic BOOLEAN DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (wallet_id) REFERENCES wallet(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+--@block
