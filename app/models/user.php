1bdf22edfd0248ca33d71896eb652e683cce7d63
HIBA53757 <hibatallahsadiki20@gmail.com>
2026-01-11 20:32:58 +0100

    WAL-5 Développement PHP – Authentification & Utilisateurs

---
+++ app/models/user.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace personalWallet\app\models;
+
+use personalWallet\app\core\Database;
+use PDO;
+
+class user
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = Database::getInstance()->getConnection();
+    }
+
+    public function create($name, $email, $password)
+    {
+        $hash = password_hash($password, PASSWORD_BCRYPT);
+
+        $stmt = $this->db->prepare(
+            "INSERT INTO `user` (name, email, password) VALUES (?, ?, ?)"
+        );
+
+        return $stmt->execute([$name, $email, $hash]);
+    }
+
+    public function checkEmail($email)
+    {
+        $stmt = $this->db->prepare(
+            "SELECT * FROM `user` WHERE email = ?"
+        );
+        $stmt->execute([$email]);
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
