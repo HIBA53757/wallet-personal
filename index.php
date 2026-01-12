871195383ffebf3e9072a38bc8b7a61cb3bfc011
HIBA53757 <hibatallahsadiki20@gmail.com>
2026-01-12 11:43:11 +0100

    WAL-10 Développement PHP – index boocle suitch

--- index.php
+++ index.php
@@ -5,12 +5,14 @@ session_start();
 use personalWallet\app\controllers\AuthContr;
 use personalWallet\app\controllers\WalletContr;
 use personalWallet\app\controllers\CategoryContr;
+use personalWallet\app\controllers\ExpenseContr;
 
 $action = $_GET['action'] ?? 'dashboard';
 
 $auth = new AuthContr();
 $walletContr = new WalletContr();
 $categoryContr = new CategoryContr();
+$expenseContr = new ExpenseContr();
 
 switch ($action) {
 
@@ -60,6 +62,18 @@ switch ($action) {
         }
         $categoryContr->addCategory();
         break;
+
+        case 'addExpense':
+    if (!isset($_SESSION['user_id']))
+         { header('Location: index.php?action=login'); exit; }
+    $expenseContr->addExpense($_SESSION['user_id']);
+    break;
+
+case 'deleteExpense':
+    if (!isset($_SESSION['user_id']))
+         { header('Location: index.php?action=login'); exit; }
+    $expenseContr->deleteExpense();
+    break;
 
     default:
         header('Location: index.php?action=dashboard');
